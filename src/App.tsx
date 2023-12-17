523955e59061d066a27bb68a5e12d8b5f4de4119
Michaël Bourjac <michael.bourjac@gmail.com>
2023-12-17 18:41:15 +0100

    feat: create base router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,22 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { AppLayout } from './layouts/AppLayout/AppLayout';
+import { HomePage } from './pages/HomePage/HomePage';
+import { ProjectDetailPage } from './pages/ProjectDetailPage/ProjectDetailPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path=":slug" element={<ProjectDetailPage />} />
+    </Route>,
+  ),
+);
+
 export const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
---
+++ src/pages/ProjectDetailPage/ProjectDetailPage.tsx
@@ -0,0 +1,3 @@
+export const ProjectDetailPage = () => {
+  return <div>ProjectDetailPage</div>;
+};
