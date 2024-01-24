079f2defabc3d10f9bf0fc45804728bb6e5c9c30
Michaël Bourjac <michael.bourjac@gmail.com>
2024-01-24 22:40:35 +0100

    refactor: use destructuring assignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,13 +19,13 @@ export const Header = ({ logo }: HeaderProps) => {
     >
       <img src={logo.imageUrl} alt={logo.alt} className="max-h-[150px]" />
       <div className="flex flex-col items-end gap-2 text-right tracking-widest">
-        {draggableModals.map((modal) => (
+        {draggableModals.map(({ id, heading }) => (
           <button
-            key={modal.id}
+            key={id}
             className="pointer-events-auto w-fit font-menu text-[2.2rem] leading-[1.4] tracking-[0.1rem] [-webkit-text-stroke:1px_#fff] [text-shadow:6px_6px_4px_rgba(0,0,0,0.3)]"
-            onClick={() => handleToggleModal(modal.id)}
+            onClick={() => handleToggleModal(id)}
           >
-            {modal.heading}
+            {heading}
           </button>
         ))}
       </div>
